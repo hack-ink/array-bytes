3cade10ba766ecc7a8e2081f93fab9d6d930f62d
Giles Cope <gilescope@gmail.com>
2022-09-07 13:24:42 +0100

    simpler

--- src/lib.rs
+++ src/lib.rs
@@ -598,8 +598,7 @@ where
 
 fn is_hex_ascii(byte: &u8) -> bool {
 	// (Tricks from std lib)
-	let digit = byte.wrapping_sub(b'0');
-	if digit < 10 {
+	if byte.wrapping_sub(b'0') < 10 {
 		return true;
 	}
 	// Force the 6th bit to be set to ensure ascii is lower case.
